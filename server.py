869530f9ffd41d9176c4f8cdde67ac9b54e7eca8
Jean-Philippe01 <jeanphilippe.neris@gmail.com>
2025-02-08 18:31:29 -0500

    Cleaned up the code to comply to  PEP8

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request 
+'''Module Docstring
+'''
+from flask import Flask, render_template, request
 from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
 app = Flask("Sentiment Analyzer")
@@ -19,23 +21,19 @@ def sent_analyzer():
     # Extract the label and score from the response
     label = response['label']
     score = response['score']
-    
     # Check if the label is None, indicating an error or invalid input
     if label is None:
         return "Invalid input! Try again."
-    else:
-        # Return a formatted string with the sentiment label and score
-        return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+    # Return a formatted string with the sentiment label and score
+    return f"The given text has been identified as {label.split('_')[1]} with a score of {score}."
 
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
-        page over the Flask channel
+        page over the Flask channel 
     '''
     return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
+    # This functions executes the flask app and deploys it on localhost:5000
     app.run(host="0.0.0.0", port=5000)
-
